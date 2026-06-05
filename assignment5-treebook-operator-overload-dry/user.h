c0a7814707b53f53f441ee41b31d35864815ee4c
Bhavay Goyal <bhavaygoyal4b@gmail.com>
2026-06-05 19:29:05 +0000

    Ass 5 part 6 (fixed)

--- assignment5-treebook-operator-overload-dry/user.h
+++ assignment5-treebook-operator-overload-dry/user.h
@@ -4,6 +4,7 @@
  * (originally by Fabio Ibanez, with modifications by Jacob Roberts-Baca).
  */
 
+#pragma once
 #include <iostream>
 #include <string>
 #include "friend_list.h"
@@ -23,11 +24,11 @@ public:
    */
   friend std::ostream& operator<<(std::ostream& os, const User& user);
 
-  // ~User() = default;
-  // User(const User& user) = default;
-  // User& operator=(const User& user) = default;
-  // User(User&& user) = delete;
-  // User& operator=(User&& user) = delete;
+  ~User() = default;
+  User(const User& user) = default;
+  User& operator=(const User& user) = default;
+  User(User&& user) = delete;
+  User& operator=(User&& user) = delete;
 
   User& operator+=(User& rhs);
   bool operator<(const User& rhs) const;
